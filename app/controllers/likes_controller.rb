4e86e25861b760cde2d830c423fd19ab5745d0da
okamokokorooo <kokoroooo0607@gmail.com>
2019-10-06 16:41:06 +0900

    いいね機能ajax通信

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,5 @@
 class LikesController < ApplicationController
+  before_action :set_program, only: [:destroy]
   def create
     @like = Like.create(user_id: current_user.id, program_id: params[:program_id])
     @likes = Like.where(program_id: params[:program_id])
@@ -11,6 +12,11 @@ class LikesController < ApplicationController
     @like.destroy
     @likes = Like.where(program_id: params[:program_id])
     @programs = Program.all
+    # binding.pry
+  end
+
+  def set_program
+    @program = Program.find(params[:id])
     # binding.pry
   end
 end
